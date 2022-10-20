a523ad09b10cfaa77859bf44f6f6955c9a312ae6
GodswillEdet <gemzeeedet@gmail.com>
2022-10-20 11:34:21 +0100

    first commit

---
+++ authapp/models.py
@@ -0,0 +1,5 @@
+import imp
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
---
+++ authapp/views.py
@@ -0,0 +1,6 @@
+from django.views.generic import TemplateView
+
+
+class SignUpView(TemplateView):
+    template_name = "signup.html"
+    
